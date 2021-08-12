601593daa21056c9660951c1680b512cda97f1b5
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-12 22:06:13 +0530

    [Harshitha] Added -> Calculating Employee wage forr a month

--- EmpWage.js
+++ EmpWage.js
@@ -1,19 +1,17 @@
-//UC 3 - Refactoring the code to Calculating Employee Wage
+//UC 4 - Calculating Employee Wage for a month
 const IS_ABSENT=0;
 const WAGE_PER_HR=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
+const MAX_WORKING_DAYS=20;
 let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-if(empCheck==IS_ABSENT){
-    console.log("Employee is Absent");
-    return;
+for(let day=0;day<MAX_WORKING_DAYS;day++)
+{
+    empCheck=Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHrs(empCheck);
 }
-else{
-    console.log("Employee is Present");
-}
-empHrs=getWorkingHrs(empCheck);
 let empWage=empHrs*WAGE_PER_HR;
+console.log("Total Hours : "+empHrs);
 console.log("Employee Wage : "+empWage);
 function getWorkingHrs(empCheck){
 switch(empCheck){
