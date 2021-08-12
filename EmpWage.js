8864da88714e31a274a3282bb71a6c6012cefa87
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-12 22:16:57 +0530

    [Harshitha] Added -> Calculating Employee wage forr a month

--- EmpWage.js
+++ EmpWage.js
@@ -1,17 +1,16 @@
 //UC 4 - Calculating Employee Wage for a month
-const IS_ABSENT=0;
 const WAGE_PER_HR=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const MAX_WORKING_DAYS=20;
-let empHrs=0;
+let totalEmpHrs=0;
 for(let day=0;day<MAX_WORKING_DAYS;day++)
 {
     empCheck=Math.floor(Math.random()*10)%3;
-    empHrs+=getWorkingHrs(empCheck);
+    totalEmpHrs+=getWorkingHrs(empCheck);
 }
-let empWage=empHrs*WAGE_PER_HR;
-console.log("Total Hours : "+empHrs);
+let empWage=totalEmpHrs*WAGE_PER_HR;
+console.log("Total Hours : "+totalEmpHrs);
 console.log("Employee Wage : "+empWage);
 function getWorkingHrs(empCheck){
 switch(empCheck){
