6a037644667ee2c0f46d3c1cdf1a17328ebc2c11
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-13 14:23:17 +0530

    [Harshitha] Added-> Storing the daily wage in an array

--- EmpWage.js
+++ EmpWage.js
@@ -1,4 +1,4 @@
-//UC 5 - Calculating Employee Wage for total working hours or numbewr of working days in a month
+//UC 6 - Storing the daily wage in an array
 const WAGE_PER_HR=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -6,15 +6,21 @@ const MAX_WORKING_DAYS=20;
 const MAX_WORKING_HRS=160;
 let totalEmpHrs=0;
 let totalWorkingDays=0;
+let empDailyWageArray=new Array(); //Initialising array
 while(totalEmpHrs<=MAX_WORKING_HRS&&totalWorkingDays<MAX_WORKING_DAYS)
 {
     totalWorkingDays++;
     empCheck=Math.floor(Math.random()*10)%3;
-    totalEmpHrs+=getWorkingHrs(empCheck);
+    let empHrs=getWorkingHrs(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArray.push(calculateDailyWage(empHrs)); //Storing the daily wage in array
 }
-let empWage=totalEmpHrs*WAGE_PER_HR;
+let empWage=calculateDailyWage(totalEmpHrs);
+console.log("Total Working Days : "+totalWorkingDays);
 console.log("Total Hours : "+totalEmpHrs);
 console.log("Employee Wage : "+empWage);
+
+//Checking the employee hours 
 function getWorkingHrs(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
@@ -26,5 +32,10 @@ switch(empCheck){
     default:
         return 0;
 }
+}
+
+//Calculating the wage
+function calculateDailyWage(empHrs){
+    return empHrs*WAGE_PER_HR;
 }
 
