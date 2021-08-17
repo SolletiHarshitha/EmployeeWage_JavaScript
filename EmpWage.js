30c7b0242295b3af93ffaf701c35da67f7f749cb
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-17 16:42:59 +0530

    [Harshitha] Added-> Calculating Wage using Arrow Functions

--- EmpWage.js
+++ EmpWage.js
@@ -1,4 +1,4 @@
-//UC 6 - Storing the daily wage in an array
+// Storing the daily wage in an array
 const WAGE_PER_HR=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -8,15 +8,17 @@ let totalEmpHrs=0;
 let totalWorkingDays=0;
 let empDailyWageArray=new Array(); //Initialising array
 let empDailyWageMap=new Map(); //Initialising Map
+let empDailyHrsMap=new Map();
 
 while(totalEmpHrs<=MAX_WORKING_HRS&&totalWorkingDays<MAX_WORKING_DAYS)
 {
     totalWorkingDays++;
-    empCheck=Math.floor(Math.random()*10)%3;
+    let empCheck=Math.floor(Math.random()*10)%3;
     let empHrs=getWorkingHrs(empCheck);
     totalEmpHrs+=empHrs;
     let dailyWage=empDailyWageArray.push(calculateDailyWage(empHrs)); //Storing the daily wage in array
     empDailyWageMap.set(totalWorkingDays,calculateDailyWage(empHrs)); //Storing the day and the daily wage in map
+    empDailyHrsMap.set(totalWorkingDays,empHrs);
 }
 let empWage=calculateDailyWage(totalEmpHrs);
 console.log("UC6 - Total Working Days : "+totalWorkingDays);
@@ -42,7 +44,7 @@ function calculateDailyWage(empHrs){
     return empHrs*WAGE_PER_HR;
 }
 
-//UC 7A - Calculate total wage using foreach traversal or reduce method
+//Calculate total wage using foreach traversal or reduce method
 let totalEmpWage=0;
 function sum(dailyWage){
     totalEmpWage+=dailyWage;
@@ -55,7 +57,7 @@ function totalWages(totalEmpWage,dailyWage){
 }
 console.log("\nUC7A Using reduce method\nEmployee Wage : "+empDailyWageArray.reduce(totalWages,0));
 
-//UC 7B - Show the day along with the daily wage
+//Show the day along with the daily wage
 let dayCount=0;
 function mapDayWithDailyWage(dailyWage){
     dayCount++;
@@ -65,7 +67,7 @@ let mapDayWithArray=empDailyWageArray.map(mapDayWithDailyWage);
 console.log("\nUC7B - Daily Wage Map");
 console.log(mapDayWithArray);
 
-//UC 7C - Show days when full time wage earned
+//Show days when full time wage earned
 function fullTimeWage(dailyWage){
     return dailyWage.includes("160");
 }
@@ -73,22 +75,22 @@ let fullTimeWageArray=mapDayWithArray.filter(fullTimeWage);
 console.log("\nUC7C - Daily Wage Filter When Full Time Wage Earned");
 console.log(fullTimeWageArray);
 
-//UC 7D - Find the first occurence when full time wage was earned
+//Find the first occurence when full time wage was earned
 console.log("\nUC7D - First occurence of full time wage earned");
 console.log(mapDayWithArray.find(fullTimeWage));
 
-//UC 7E - Check if every element of full time wage is truly holding full time wage
+//Check if every element of full time wage is truly holding full time wage
 console.log("\nUC7E - Check all elements having full time wage");
 console.log(fullTimeWageArray.every(fullTimeWage));
 
-//UC 7F - Check if any part time wage
+//Check if any part time wage
 function partTimeWage(dailyWage){
     return dailyWage.includes("80");
 }
 console.log("\nUC7F - Check if any part time wage");
 console.log(mapDayWithArray.some(partTimeWage));
 
-//UC 7G - Find the number of days employee worked
+//Find the number of days employee worked
 function totalDaysWorked(numOfDays,dailyWage){
     if(dailyWage>0)
     return numOfDays+1;
@@ -96,7 +98,15 @@ function totalDaysWorked(numOfDays,dailyWage){
 }
 console.log("\nUC7G - Number of Days Worked : "+empDailyWageArray.reduce(totalDaysWorked,0));
 
-//UC 8 - Storing the day along with daily wage in map
+//Storing the day along with daily wage in map
 console.log("\nEmployee Wage Map :");
 console.log(empDailyWageMap);
 console.log("Employee Wage : "+ Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+//Arrow Functions
+const findTotalWage = (totalWage,dailyWage) => {
+    return totalWage+dailyWage;
+}
+let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotalWage,0);
+let totalWage=empDailyWageArray.filter(dailyWage => dailyWage > 0).reduce(findTotalWage,0);
+console.log("Employee Wage Using Arrow Function : \nTotal Hours : "+totalHours+"\nTotal Wage : "+totalWage);
